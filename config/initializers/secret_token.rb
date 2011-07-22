c74089cc7733f347cceb3d826c91195e40544007
Kyle Bolton <kyle.bolton@gmail.com>
2011-07-22 16:34:35 -0400

    Upgrade application from 2.3.8 to 3.0.9

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '21ddf918b2d2b9613d0385fa9d9388c0fb0287d78afda88e98fe214af3895d93e60d6cf3f2870bb1e6da320fcc95fcf58fb9255cab2f15c7ec6321f162052373';
+CloudEdit::Application.config.secret_token = '67adddabd35700481561ba93ca5fd87a88a1c95b4e705e833aa61b9debb15b8cb25a247fd1a1f3b8200ec844a86ec97292d3f7b84af619f94044eda91e9dd13d'
